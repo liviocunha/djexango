68b5a7983c75debc6c76cdcc2c33407cfd414b68
Lívio Cunha <liviocunha91@hotmail.com>
2020-12-18 20:27:33 -0300

    Fix settings.py and requirements

--- djexango/settings.py
+++ djexango/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'djexango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.slite3')
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
 DATABASES = {
     'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
